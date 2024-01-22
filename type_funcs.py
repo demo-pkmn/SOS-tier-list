71dca2d4053f446571950e548dfee69df0186b50
MQmdm <mmdemorentin@gmail.com>
2024-01-22 20:31:54 +0100

    typing

--- type_funcs.py
+++ type_funcs.py
@@ -2,6 +2,7 @@ from load import TYPES, MON_TYPES
 from mon_funcs import mon_id_series_to_mon_name_series
 import pandas as pd
 from functools import reduce
+from typing import List
 
 def type_name_to_id(type_name: str) -> int:
     return TYPES[TYPES['identifier']==type_name]['id'].iloc[0]
@@ -9,14 +10,14 @@ def type_name_to_id(type_name: str) -> int:
 def type_id_to_mon_id_series(type_id: int) -> pd.Series:
     return MON_TYPES[MON_TYPES['type_id']==type_id]['pokemon_id']
 
-def type_list() -> list[str]:
+def type_list() -> List[str]:
     return TYPES[TYPES['id']<10000]['identifier'].tolist()
 
-def type_list_to_mon_name_series(type_list: list[str]) -> pd.Series:
+def type_list_to_mon_name_series(type_list: List[str]) -> pd.Series:
     if not type_list: return None
     mon_id_series_list = [type_id_to_mon_id_series(
             type_name_to_id(type_name)
         ) for type_name in type_list
     ]
-    mon_id_series = reduce(lambda x, y: pd.Series(list(set(x) & set(y))), mon_id_series_list)
+    mon_id_series = reduce(lambda x, y: pd.Series(List(set(x) & set(y))), mon_id_series_list)
     return mon_id_series_to_mon_name_series(mon_id_series).values
