5d59ea5ec82768c94a9ff82e8769aef32ba42d63
MQmdm <mmdemorentin@gmail.com>
2024-01-22 20:28:09 +0100

    typing

--- ability_funcs.py
+++ ability_funcs.py
@@ -1,6 +1,7 @@
 from load import ABILITIES, MON_ABILITIES
 from mon_funcs import mon_id_series_to_mon_name_series
 import pandas as pd
+from typing import List
 from functools import reduce
 
 def ability_name_to_id(ability_name: str) -> int:
@@ -12,7 +13,7 @@ def ability_id_to_mon_id_series(ability_id: int) -> pd.Series:
 def ability_list():
     return ABILITIES[(ABILITIES['local_language_id']==9) & (ABILITIES['ability_id']<10000)]['name'].tolist()
 
-def ability_list_to_mon_name_series(ability_list: list[str]) -> pd.Series:
+def ability_list_to_mon_name_series(ability_list: List[str]) -> pd.Series:
     if not ability_list: return None
     mon_id_series_list = [ability_id_to_mon_id_series(
             ability_name_to_id(ability_name)
